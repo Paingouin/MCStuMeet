1ddcf2dd47d047ac797a8e90d82f67d272abd23f
OzgunBas94 <oezguen.bas@student.reutlingen-university.de>
2016-12-15 14:31:59 +0100

    added regstration (added) activity

---
+++ app/src/main/java/com/example/cogo/mcstumeet/RegistrationInput.java
@@ -0,0 +1,13 @@
+package com.example.cogo.mcstumeet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistrationInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_input);
+    }
+}
