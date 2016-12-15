0ce2fabe056097f0500b7b26a4b4fb9bf263ae72
OzgunBas94 <oezguen.bas@student.reutlingen-university.de>
2016-12-15 15:20:42 +0100

    added intent

--- app/src/main/java/com/example/cogo/mcstumeet/RegistrationInterests.java
+++ app/src/main/java/com/example/cogo/mcstumeet/RegistrationInterests.java
@@ -2,6 +2,10 @@ package com.example.cogo.mcstumeet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class RegistrationInterests extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class RegistrationInterests extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_interests);
+    }
+
+    public void passData(View view){
+        Button female = (Button) findViewById(R.id.interests_female);
+        Intent intent = new Intent(this, RegistrationInput.class);
+        if(view == female){
+            intent.putExtra("interests", "female");
+        } else {
+            intent.putExtra("interests", "male");
+        }
+        startActivity(intent);
     }
 }
