5b9d92bb53525b9abee819c4821ec8c8432ff0c2
Gamze Dogan <gamze.dogan@student.reutlingen-university.de>
2016-12-15 15:50:16 +0100

    registration

--- app/src/main/java/com/example/cogo/mcstumeet/RegistrationInterests.java
+++ app/src/main/java/com/example/cogo/mcstumeet/RegistrationInterests.java
@@ -1,13 +1,33 @@
 package com.example.cogo.mcstumeet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationInterests extends AppCompatActivity {
+    String data_gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle extras = getIntent().getExtras();
+        String data = extras.getString("gender");
+        data_gender = data;
         setContentView(R.layout.activity_registration_interests);
     }
+
+   // public void passData(View view){
+        //Button girls = (Button) findViewById(R.id.interests_girl);
+        //Intent intent = new Intent(this, RegistrationInput.class);
+        //if(view == girls){
+         //   intent.putExtra("gender", data_gender);
+         //   intent.putExtra("interests", "girls");
+        //} else {
+        //    intent.putExtra("gender", data_gender);
+        //    intent.putExtra("interests", "guys");
+        //}
+        //startActivity(intent);
+    //}
 }
