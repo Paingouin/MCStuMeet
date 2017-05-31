68bf4fa84fc15439f93c2fcb616e98ffe6e6d265
Paingouin <jordan.busiere21@gmail.com>
2017-05-31 11:03:50 +0200

    test

--- app/src/main/java/com/example/cogo/mcstumeet/base64/Base.java
+++ app/src/main/java/com/example/cogo/mcstumeet/base64/Base.java
@@ -9,15 +9,22 @@ import java.io.ByteArrayOutputStream;
 public class Base {
 
     public Bitmap convertStringToBitmap(String dbImage){
-        byte [] encodeByte= Base64.decode(dbImage,Base64.DEFAULT);
+
+        //NOTE(jordan)  :test
+        byte[] encodeByte= Base64.decode(dbImage,Base64.DEFAULT);
         Bitmap bitmap= BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
         return bitmap;
     }
 
+
+
+
     public String convertBitmapToString(Bitmap bitmap){
         ByteArrayOutputStream baos = new  ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
-        byte [] b=baos.toByteArray();
+
+        byte[] b = baos.toByteArray();
+
         String temp= Base64.encodeToString(b, Base64.DEFAULT);
        return temp;
     }
