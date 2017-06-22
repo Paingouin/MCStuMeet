adbcb00e59f948f3a3ae290848899f5494e15045
Benigne21 <benigne.thierry@stjodijon.com>
2017-06-22 14:20:49 +0200

    Start locales

--- app/src/main/java/com/example/cogo/mcstumeet/registration/RegistrationGender.java
+++ app/src/main/java/com/example/cogo/mcstumeet/registration/RegistrationGender.java
@@ -3,13 +3,24 @@ package com.example.cogo.mcstumeet.registration;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.LocaleList;
 
 import com.example.cogo.mcstumeet.R;
 import com.example.cogo.mcstumeet.login.LogIn;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class RegistrationGender extends AppCompatActivity {
+ImageView imageView_fr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +28,28 @@ public class RegistrationGender extends AppCompatActivity {
         setContentView(R.layout.activity_registration_gender);
     }
 
-    public void passData(View view){
+
+
+    public void FRonClick(View view) {
+
+
+                Configuration config = new Configuration();
+            config.locale = Locale.FRENCH;
+
+            Log.e("langue", "changée");
+
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        Intent intent = new Intent(this, RegistrationGender.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+    }
+
+    public void passData(View view) {
         Button female = (Button) findViewById(R.id.female_button);
         Intent intent = new Intent(this, RegistrationInterests.class);
 
-        if(view == female){
+        if (view == female) {
             intent.putExtra("gender", "female");
         } else {
             intent.putExtra("gender", "male");
@@ -31,7 +59,7 @@ public class RegistrationGender extends AppCompatActivity {
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
-    public void singIn(View view){
+    public void singIn(View view) {
         Button female = (Button) findViewById(R.id.signinStuMeet_button);
         Intent intent = new Intent(this, LogIn.class);
         startActivity(intent);
